05a08b41e5f5613787a4724577d70ce69ba6a0c8
Kamil Błoński <kblonski02@gmail.com>
2026-07-14 00:28:59 +0200

    .

---
+++ domain/analysis/knowledge_analysis_questions.py
@@ -0,0 +1,131 @@
+
+KNOWLEDGE_ANALYSIS_QUESTIONS = [
+    "Jaki konkretny problem rozwiązuje produkt? Opisz problem klienta, jego skalę, częstotliwość występowania oraz poziom uciążliwości w skali 1-10.",
+    "Jak istotny jest problem dla klienta? Oceń, czy jest to drobna niedogodność, realna frustracja czy pilna potrzeba wymagająca rozwiązania.",
+    "Jakie emocje wywołuje ten problem u klienta i jakie są główne motywacje zakupu produktu?",
+    "Czy klient aktywnie szuka rozwiązania tego problemu? Oceń poziom intencji zakupowej i prawdopodobieństwo zakupu.",
+    "Czy produkt jest typu must have czy nice to have? Uzasadnij ocenę.",
+    "Czy produkt ma potencjał do zakupu impulsywnego? Oceń, jakie czynniki mogą powodować szybki zakup.",
+    "Jak wygląda konkurencja dla tego produktu? Wymień istniejące produkty konkurencyjne, alternatywne rozwiązania oraz głównych konkurentów.",
+    "Czym produkt różni się od konkurencji? Oceń, czy posiada wyraźną przewagę konkurencyjną i jaka ona jest.",
+    "Jak często klient może spotykać się z problemem rozwiązywanym przez produkt? Oceń, czy częstotliwość problemu sprzyja wysokiej sprzedaży.",
+    "Czy produkt posiada efekt WOW? Oceń, czy wyróżnia się na rynku i czy łatwo stworzyć atrakcyjne materiały reklamowe.",
+    "Kim jest idealny klient tego produktu? Opisz grupę docelową, potrzeby, motywacje oraz zachowania zakupowe.",
+    "Czy produkt dobrze prezentuje się wizualnie? Oceń potencjał sprzedaży poprzez zdjęcia i materiały video.",
+    "Jaka powinna być cena sprzedaży produktu? Uwzględnij koszt zakupu + 15 zł dostawy + 20 zł reklamy + 15 zł marży.",
+    "Czy produkt ma potencjał osiągnięcia dobrej rentowności?",
+    "Czy produkt sprawia wrażenie wysokiej jakości?",
+    "Jakie jest ryzyko reklamacji, zwrotów i niezadowolenia klientów?",
+    "Czy produkt jest łatwy w logistyce?",
+    "Czy produkt można sprzedawać cały rok? Oceń sezonowość.",
+    "Czy klient może kupić produkt ponownie? Oceń potencjał dodatków i akcesoriów.",
+    "Czy produkt wymaga elektroniki, specjalnej obsługi, certyfikatów lub zezwoleń?",
+    "Oceń ogólny potencjał produktu w skali 1-10.",
+    "Jakie są największe zalety produktu?",
+    "Jakie są największe ryzyka sprzedaży tego produktu?",
+    "Co należy poprawić przed rozpoczęciem sprzedaży?",
+    "Czy rekomendujesz sprzedaż produktu? TAK/WARUNKOWO/NIE i dlaczego?"
+]
+
+# PRODUCT_ANALYSIS_QUESTIONS = [
+
+#     "Jaki konkretny problem rozwiązuje produkt?",
+
+#     "Czy jest to realny i istotny problem dla klientów?",
+
+#     "Jak duża jest potrzeba rozwiązania tego problemu?",
+
+#     "Jak bardzo uciążliwy jest problem dla klienta? Oceń w skali 1-10.",
+
+#     "Czy jest to drobna niedogodność czy realna frustracja?",
+
+#     "Jakie emocje wywołuje ten problem u klienta?",
+
+#     "Czy klient aktywnie szuka rozwiązania?",
+
+#     "Czy produkt jest must have czy nice to have?",
+
+#     "Czy produkt może generować zakup impulsywny?",
+
+
+#     "Czy istnieją produkty konkurencyjne?",
+
+#     "Jakie produkty konkurencyjne istnieją?",
+
+#     "Czym produkt różni się od konkurencji?",
+
+#     "Czy produkt posiada przewagę konkurencyjną?",
+
+
+#     "Jak często klient spotyka się z tym problemem?",
+
+#     "Czy częstotliwość problemu sprzyja wysokiej sprzedaży?",
+
+
+#     "Czy produkt posiada efekt WOW?",
+
+#     "Czy produkt wyróżnia się na rynku?",
+
+#     "Czy łatwo stworzyć atrakcyjne reklamy produktu?",
+
+
+#     "Kto jest idealnym klientem tego produktu?",
+
+#     "Jak duża jest grupa docelowa?",
+
+#     "Jakie są potrzeby i motywacje klientów?",
+
+
+#     "Czy produkt dobrze wygląda na zdjęciach i filmach?",
+
+#     "Czy nadaje się do reklam TikTok/Facebook/Instagram?",
+
+
+#     "Jaka cena sprzedaży będzie atrakcyjna dla klienta?",
+
+#     "Oblicz minimalną cenę sprzedaży według wzoru: cena zakupu + 15 zł dostawy + 20 zł reklamy + 15 zł marży.",
+
+#     "Czy klient będzie skłonny zapłacić minimalną cenę sprzedaży?",
+
+#     "Zaproponuj realistyczną cenę sprzedaży.",
+
+#     "Czy produkt ma potencjał osiągnięcia dobrej rentowności?",
+
+
+#     "Czy produkt sprawia wrażenie wysokiej jakości?",
+
+#     "Czy istnieje ryzyko reklamacji i zwrotów?",
+
+
+#     "Czy produkt jest łatwy w wysyłce?",
+
+#     "Czy istnieje ryzyko uszkodzeń podczas transportu?",
+
+
+#     "Czy produkt można sprzedawać cały rok?",
+
+#     "Czy występuje sezonowość?",
+
+
+#     "Czy klient kupi produkt ponownie?",
+
+#     "Czy można sprzedawać dodatki lub akcesoria?",
+
+
+#     "Czy produkt wymaga elektroniki lub specjalnej obsługi?",
+
+#     "Czy produkt wymaga certyfikatów lub zezwoleń?",
+
+
+#     "Oceń potencjał produktu w skali 1-10.",
+
+#     "Jakie są największe zalety produktu?",
+
+#     "Jakie są największe ryzyka sprzedaży?",
+
+#     "Co należy poprawić przed rozpoczęciem sprzedaży?",
+
+#     "Czy rekomendujesz sprzedaż produktu? TAK/WARUNKOWO/NIE. Dlaczego?"
+# ]
+
+
