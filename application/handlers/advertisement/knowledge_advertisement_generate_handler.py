e79c62a7dbd86492bc32fa49aafd382d65e92cef
root <root@DESKTOP-0G32UDC.localdomain>
2026-07-16 10:19:00 +0200

    .

--- application/handlers/advertisement/knowledge_advertisement_generate_handler.py
+++ application/handlers/advertisement/knowledge_advertisement_generate_handler.py
@@ -1,10 +1,16 @@
+import json
 from di.container import Container
+from domain.models.ollama.llm_ollama_message import LlmOllamaMessage
+from domain.enums.ollama_message_role import OllamaMessageRole
+
 
 def knowledge_advertisement_generate_handler(knowledge_id : int ):
     container = Container()
     
     knowledge_service = container.knowledge_service()
+    ollama_service = container.ollama_service()
     
     knowledge_details = knowledge_service.get_knowledge_details_by_id(knowledge_id=knowledge_id)
+    knowledge_details_json = json.loads( knowledge_details.to_dict() )
     
-    return "Knowledge advetisement generate"
+    return "knowledge_details.to_dict()"
