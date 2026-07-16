b22b6f3dc7f09ae7408b9b73bda30a13120a6573
root <root@DESKTOP-0G32UDC.localdomain>
2026-07-16 10:14:17 +0200

    .

--- application/handlers/advertisement/knowledge_advertisement_generate_handler.py
+++ application/handlers/advertisement/knowledge_advertisement_generate_handler.py
@@ -1,4 +1,10 @@
 from di.container import Container
 
 def knowledge_advertisement_generate_handler(knowledge_id : int ):
+    container = Container()
+    
+    knowledge_service = container.knowledge_service()
+    
+    knowledge_details = knowledge_service.get_knowledge_details_by_id(knowledge_id=knowledge_id)
+    
     return "Knowledge advetisement generate"
